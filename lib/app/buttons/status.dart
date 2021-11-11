10ec2d162d43096cb22ba9d31b0a3e454a45274e
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-12 01:07:49 +0330

    add status class button for clear

---
+++ lib/app/buttons/status.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class StatusButons extends StatelessWidget {
+  StatusButons({@required this.option});
+  final option;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text(
+        option,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      elevation: 0.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+            Radius.circular(20.0)
+        ),
+        side: BorderSide(
+            color: Colors.amber,
+        ),
+      ),
+      color: Colors.amber,
+      onPressed: () {},
+    );
+  }
+}
