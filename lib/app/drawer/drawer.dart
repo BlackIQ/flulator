0f1347bef93cb72a8be6da645a219731aad28dec
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-17 12:05:17 +0330

    Add Drawer widget

---
+++ lib/app/drawer/drawer.dart
@@ -0,0 +1,33 @@
+import 'package:calculator/app/drawer/about.dart';
+import 'package:calculator/app/drawer/item.dart';
+import 'package:flutter/material.dart';
+
+import 'head.dart';
+
+class FluDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 15.0,
+          ),
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                DrawerHead(),
+                Divider(color: Colors.teal),
+                AboutItem(),
+                DrawerItem(icon: Icons.phone, text: "Conact", onTap: () {}),
+                DrawerItem(icon: Icons.exit_to_app, text: "Exit", onTap: () {}),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
