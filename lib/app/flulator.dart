a086eb0b15e944c1f8efba53579a2622993448d8
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-17 23:31:03 +0330

    Add texts

--- lib/app/flulator.dart
+++ lib/app/flulator.dart
@@ -7,6 +7,7 @@ class Flulator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       drawer: FluDrawer(),
       appBar: AppBar(
         title: Text(
@@ -15,12 +16,158 @@ class Flulator extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: FluButton(
-          text: 'Amir',
-          fillColor: Colors.teal,
-          textColor: Colors.white,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(right: 10),
+            child: Text(
+              '123*123',
+              style: GoogleFonts.boogaloo(
+                fontSize: 25,
+                color: Colors.grey,
+              ),
+            ),
+            alignment: Alignment(1, 1),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 10),
+            child: Text(
+              '123',
+              style: GoogleFonts.boogaloo(
+                fontSize: 50,
+                color: Colors.teal,
+              ),
+            ),
+            alignment: Alignment(1, 1),
+          ),
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: 'AC',
+                fillColor: Colors.grey,
+                textColor: Colors.white,
+              ),
+              FluButton(
+                text: 'C',
+                fillColor: Colors.grey,
+                textColor: Colors.white,
+              ),
+              FluButton(
+                text: '%',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+              ),
+              FluButton(
+                text: '/',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '7',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '8',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '9',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '*',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '4',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '5',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '6',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '-',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '1',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '2',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '3',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '+',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '.',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '0',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '00',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+              ),
+              FluButton(
+                text: '=',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
