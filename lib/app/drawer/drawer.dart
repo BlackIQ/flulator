3f5e59e8908d80547d2b52eebe6c05cf28fac387
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-11-18 00:35:11 +0330

    Working on drawer

--- lib/app/drawer/drawer.dart
+++ lib/app/drawer/drawer.dart
@@ -1,5 +1,4 @@
 import 'package:calculator/app/drawer/items/about.dart';
-import 'package:calculator/app/drawer/items/contact.dart';
 import 'package:calculator/app/drawer/items/exit.dart';
 import 'package:calculator/app/drawer/items/head.dart';
 import 'package:flutter/material.dart';
@@ -21,8 +20,11 @@ class FluDrawer extends StatelessWidget {
                 DrawerHead(),
                 Divider(color: Colors.teal),
                 AboutItem(),
-                ContactItem(),
                 ExitItem(),
+                Spacer(),
+                Text(
+                  'Hi'
+                ),
               ],
             ),
           ),
