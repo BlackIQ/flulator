c3a89bec4f39acaf0ca88bc725279dbe32ab5177
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-02 12:42:31 +0330

    Update about developer

--- lib/app/pages/developer.dart
+++ lib/app/pages/developer.dart
@@ -13,34 +13,47 @@ class Developer extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Amirhossein Mohammadi',
-            style: GoogleFonts.boogaloo(
-              color: Colors.teal,
-              fontSize: 40,
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Amirhossein Mohammadi',
+              style: GoogleFonts.boogaloo(
+                color: Colors.teal,
+                fontSize: 40,
+              ),
             ),
-          ),
-          Text(
-            'Full stack developer',
-            style: GoogleFonts.boogaloo(
-              color: Colors.teal[300],
-              fontSize: 25,
+            Text(
+              'Full stack developer',
+              style: GoogleFonts.boogaloo(
+                color: Colors.teal[300],
+                fontSize: 25,
+              ),
             ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Text(
-            'BlackIQ.ir',
-            style: GoogleFonts.boogaloo(
-              color: Colors.teal,
-              fontSize: 20,
+            SizedBox(
+              height: 20,
             ),
-          ),
-        ],
+            Text(
+              'You can check this site to see more information about me :)',
+              style: GoogleFonts.boogaloo(
+                color: Colors.teal,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'BlackIQ.ir',
+              style: GoogleFonts.boogaloo(
+                color: Colors.teal,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
