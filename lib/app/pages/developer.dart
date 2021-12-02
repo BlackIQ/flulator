0e38e139a20c7031c694de9ef846b514de74d3d5
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-02 12:36:38 +0330

    Create a part for developer in another dir

--- lib/app/pages/developer.dart
+++ lib/app/pages/developer.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'drawer/drawer.dart';
-
 class Developer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      drawer: FluDrawer(),
       appBar: AppBar(
         title: Text(
           'About developer',
@@ -19,34 +16,28 @@ class Developer extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Center(
-            child: Column(
-              children: <Widget>[
-                Text(
-                  'Amirhossein Mohammadi',
-                  style: GoogleFonts.boogaloo(
-                    color: Colors.teal,
-                    fontSize: 40,
-                  ),
-                ),
-                Text(
-                  'Full stack developer',
-                  style: GoogleFonts.boogaloo(
-                    color: Colors.teal[300],
-                    fontSize: 25,
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  'BlackIQ.ir',
-                  style: GoogleFonts.boogaloo(
-                    color: Colors.teal,
-                    fontSize: 20,
-                  ),
-                ),
-              ],
+          Text(
+            'Amirhossein Mohammadi',
+            style: GoogleFonts.boogaloo(
+              color: Colors.teal,
+              fontSize: 40,
+            ),
+          ),
+          Text(
+            'Full stack developer',
+            style: GoogleFonts.boogaloo(
+              color: Colors.teal[300],
+              fontSize: 25,
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'BlackIQ.ir',
+            style: GoogleFonts.boogaloo(
+              color: Colors.teal,
+              fontSize: 20,
             ),
           ),
         ],
