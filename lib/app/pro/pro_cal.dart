e78844b29dc14112f33a0a7c0fd3b5b52892daa7
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-08 12:44:34 +0330

    Create pro sample

---
+++ lib/app/pro/pro_cal.dart
@@ -0,0 +1,284 @@
+import 'package:flulator/app/widgets/buttons/calculator_buttons.dart';
+import 'package:flulator/app/widgets/drawer/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class ProCalculator extends StatefulWidget {
+  @override
+  ProCalculatorState createState() => ProCalculatorState();
+}
+
+class ProCalculatorState extends State<ProCalculator> {
+  String _history = 'Welcome to Pro!';
+  String _expression = 'Hi, Press AC to start app.';
+
+  void numClick(String text) {
+    setState(() {
+      _expression += text;
+    });
+  }
+
+  void deLast(String text) {
+    setState(() {
+      List<String> c = _expression.split("");
+      c.removeLast();
+      _expression = c.join();
+    });
+  }
+
+  void clear(String text) {
+    setState(() {
+      if (text == 'AC') {
+        _expression = '';
+        _history = '';
+      } else if (text == 'C') {
+        _expression = '';
+      }
+    });
+  }
+
+  void evaluation(String text) {
+    Parser p = Parser();
+    Expression exp = p.parse(_expression);
+    ContextModel cm = ContextModel();
+
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    setState(() {
+      _history = eval.toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      drawer: FluDrawer(),
+      appBar: AppBar(
+        elevation: 0,
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Normal Calculator',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ],
+        title: Text(
+          'Pro Flulator',
+          style: GoogleFonts.boogaloo(),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(right: 10),
+            child: Text(
+              _expression,
+              style: GoogleFonts.boogaloo(
+                fontSize: 25,
+                color: Colors.grey,
+              ),
+            ),
+            alignment: Alignment(1, 1),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 10),
+            child: Text(
+              _history,
+              style: GoogleFonts.boogaloo(
+                fontSize: 50,
+                color: Colors.teal,
+              ),
+            ),
+            alignment: Alignment(1, 1),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '-',
+                fillColor: Colors.blueGrey[300],
+                textColor: Colors.white,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '+',
+                fillColor: Colors.blueGrey[300],
+                textColor: Colors.white,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '(',
+                fillColor: Colors.blueGrey[300],
+                textColor: Colors.white,
+                callback: numClick,
+              ),
+              FluButton(
+                text: ')',
+                fillColor: Colors.blueGrey[300],
+                textColor: Colors.white,
+                callback: numClick,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: 'AC',
+                fillColor: Colors.grey,
+                textColor: Colors.white,
+                callback: clear,
+              ),
+              FluButton(
+                text: 'C',
+                fillColor: Colors.grey,
+                textColor: Colors.white,
+                callback: clear,
+              ),
+              FluButton(
+                text: '<',
+                fillColor: Colors.grey,
+                textColor: Colors.white,
+                callback: deLast,
+              ),
+              FluButton(
+                text: '/',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+                callback: numClick,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '7',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '8',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '9',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '*',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+                callback: numClick,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '4',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '5',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '6',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '-',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+                callback: numClick,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '1',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '2',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '3',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '+',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+                callback: numClick,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              FluButton(
+                text: '.',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '0',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '00',
+                fillColor: Colors.white,
+                textColor: Colors.teal,
+                callback: numClick,
+              ),
+              FluButton(
+                text: '=',
+                fillColor: Colors.blueGrey,
+                textColor: Colors.white,
+                callback: evaluation,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
