1376abdb15cd15a55829a4b52aa767849be8cccc
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-08 13:09:34 +0330

    Add probutton

---
+++ lib/app/widgets/buttons/pro_buttons.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ProButton extends StatelessWidget {
+  ProButton({
+    @required this.text,
+    @required this.textColor,
+    @required this.fillColor,
+    @required this.callback,
+  });
+
+  final text;
+  final textColor;
+  final fillColor;
+  final Function callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: SizedBox(
+        child: FlatButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+          color: fillColor,
+          child: Text(
+            text,
+            style: GoogleFonts.boogaloo(
+              color: textColor,
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+          onPressed: () {
+            callback(text);
+          },
+        ),
+        width: 50,
+        height: 50,
+      ),
+    );
+  }
+}
